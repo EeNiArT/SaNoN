f29b2b0056dba5c870669f978ef60e5ea641cac3
daudriaz <35921960+daudriaz@users.noreply.github.com>
2018-02-12 16:21:49 +0500

    12 Feb 2018
    Full Project

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace HomePractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+        public ActionResult AboutUs()
+        {
+
+            return View();
+        }
+        public ActionResult Gallery()
+        {
+
+            return View();
+        }
+        public ActionResult ContactUs()
+        {
+
+            return View();
+        }
+
+    }
+}
